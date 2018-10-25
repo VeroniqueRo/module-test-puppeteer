ee8c602801f78b066858450f56ee75a84334297b
VeroniqueRo <veronique.rouault@campus-numerique-in-the-alps.com>
2018-10-25 16:36:48 +0200

    Ajout test de génération du QR Code + retour vers la page d'accueil

--- tests/qrcode-generate-anonymous.test.js
+++ tests/qrcode-generate-anonymous.test.js
@@ -1,6 +1,6 @@
 const timeout = 15000
 
-// test de la génération d'un qr code
+// test de la page de génération d'un qr code
 describe("QR code generate", () => {
     let page
 
@@ -25,6 +25,33 @@ describe("QR code generate", () => {
         await page.screenshot({ path: './tests/img/img.png' });
         // on vérifie qu'il contient une image
         expect(html).toContain('img')
+
+    }, timeout)
+
+    // Vérification du retour vers la page d'accueil
+    test('basic qrcode generate + home return', async () => {
+
+        // Génération d'une url courte
+        await page.goto('http://polr.campus-grenoble.fr')
+        await page.waitForSelector('.long-link-input')
+        await page.type('.long-link-input', 'https://www.google.com/search?source=hp&ei=QQbPW52GC9CRlwSHw46oAg&q=puppeteer+jest&oq=puppeteer+jest&gs_l=psy-ab.3...2441.6095.0.6926.0.0.0.0.0.0.0.0..0.0....0...1c.1.64.psy-ab..0.0.0....0.qKd5wLlrTYk');
+        await page.$eval('#shorten', el => el.click());
+        await page.waitForSelector('#short_url')
+        await page.screenshot({ path: './tests/img/shorturl-for-qrcode.png' });
+        // on attend que le bouton "Générate QR Code" soit affiché
+        await page.waitForSelector('#generate-qr-code')
+        await page.$eval('#generate-qr-code', el => el.click());
+        // on attent que l'élément ".qr-code-container" soit chargé
+        await page.waitForSelector('.btn-info')
+        // cliquer sur le bouton "Shorten nother"
+        await page.$eval('.btn-info', el => el.click());
+        // attendre que l'élément <body> soit chargé
+        await page.waitForSelector('body')
+        // récupérer le contenu de l'élément <body>
+        const html = await page.$eval('body', e => e.innerHTML)
+        // vérifier que dans cet élément Body on trouve "Polr du campus"
+        await page.screenshot({ path: './tests/img/basic-home-return.png' });
+        expect(html).toContain("Polr du campus")
 
     }, timeout)
 
---
+++ tests/qrcode-verification.test.js
@@ -0,0 +1,54 @@
+const timeout = 15000
+
+// test de la génération d'un qr code
+describe("QR code verification", () => {
+    let page
+
+    // Vérification de la présence d'une image quand on crée un QR Code
+    test('QR code generate', async () => {
+
+        // Génération d'une url courte
+        await page.goto('http://polr.campus-grenoble.fr')
+        await page.waitForSelector('.long-link-input')
+        await page.type('.long-link-input', 'https://zxing.org/w/decode.jspx');
+        await page.$eval( '#shorten', el => el.click() );
+        await page.waitForSelector('#short_url')
+        await page.screenshot({ path: './tests/img/shorturl-for-qrcode.png' });
+        // Récupere la valeur du champs "short url"
+        const valurl = await page.$eval('#short_url', el => el.value)
+        // on attend que le bouton "Générate QR Code" soit affiché
+        await page.waitForSelector('#generate-qr-code')
+        await page.$eval('#generate-qr-code', el => el.click());
+        // on attent que l'élément ".qr-code-container" soit chargé
+        await page.waitForSelector('.qr-code-container')
+        // on récupère la source de l'image générée
+        const imgs = await page.$$eval('img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+        await page.goto('https://zxing.org/w/decode.jspx')
+        await page.waitForSelector('#upload')
+        //await page.type('input[type="text"]', imgs);
+        await page.type('input[name="u"]', imgs);
+        await page.screenshot({ path: './tests/img/test.png' });
+        await page.$eval('#upload tr:first-child input[type=submit]', el => el.click());
+        await page.waitForSelector('#result');
+        //const button = await page.$('form[method="get"] > input [type="submit"]');
+        //await button.click();
+        //await page.waitFor(20000);
+        //await page.keyboard.press('Enter');
+        //await page.keyboard.press('Enter');
+        await page.screenshot({
+            path: './tests/img/test2.png',
+            fullPage: true});
+      
+                
+        
+
+    }, timeout)
+
+        // cette fonction est lancée avant chaque test de cette
+    // série de tests
+    beforeAll(async () => {
+        // ouvrir un onglet dans le navigateur
+        page = await global.__BROWSER__.newPage()
+    }, timeout)
+
+})
