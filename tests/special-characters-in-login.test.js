10b4d326edddc4863f4cb842acaae8863d6aa51d
VeroniqueRo <veronique.rouault@campus-numerique-in-the-alps.com>
2018-11-21 17:06:25 +0100

    Test d'ajout de login avec characteres spéciaux

--- tests/special-characters-in-login.test.js
+++ tests/special-characters-in-login.test.js
@@ -1,4 +1,5 @@
 const timeout = 15000
+const connectUrl = 'http://localhost:8000/' //http://polr.campus-grenoble.fr
 
 // test d'un raccourcisseur d'URL
 describe("Special characters in login", () => {
@@ -6,7 +7,7 @@ describe("Special characters in login", () => {
 
     // vérification d'une inscription avec login ayant un caractère spécial
     test('register with special login', async () => {
-        await page.goto('http://polr.campus-grenoble.fr')
+        await page.goto(connectUrl)
         await page.waitForSelector('#navbar li a')
         // click sur le lien "Sign Up" de la navigation
         await page.evaluate( () => {
@@ -24,30 +25,29 @@ describe("Special characters in login", () => {
         // on clique sur le bouton pour s'enregistrer
         await page.$eval('.btn-default', el => el.click());
         await page.screenshot({ path: './tests/img/special-characters-in-login/sign-up-input2.png' });
-        await page.goto('http://polr.campus-grenoble.fr/admin#admin')
-        //on tape un texte dans la barre de recherche avec un delai de réponse
-        await page.type('#admin_users_table_filter input[type = "search"]', "Vero1%", {delay:50})
-        const btndelete = await page.$eval('#admin_users_table tr td a.btn-danger', e => e.innerHTML)
-        // on vérifie qu'il contient la bonne chaîne de caractères
-        expect(btndelete).toContain("Delete")
+        // await page.goto('http://polr.campus-grenoble.fr/admin#admin')
+        // //on tape un texte dans la barre de recherche avec un delai de réponse
+        // await page.type('#admin_users_table_filter input[type = "search"]', "Vero1%", {delay:50})
+        // const btndelete = await page.$eval('#admin_users_table tr td a.btn-danger', e => e.innerHTML)
+        // // on vérifie qu'il contient la bonne chaîne de caractères
+        // expect(btndelete).toContain("Delete")
         //on clique sur le bouton
-        await page.$eval('#admin_users_table tr td a.btn-danger', el => el.click());    }, timeout)
-   
-        // // on attent que l'élément ".form-control" soit chargé
-        // await page.waitForSelector('form[action="/login"]')
-        // // on saisit les données dans le formulaire Register
-        // await page.type('form[action="/login"] input[name="username"]',"Vero1%")
-        // await page.type('form[action="/login"] input[name="password"]',"test")
-        // // on clique sur le bouton pour s'enregistrer
-        // await page.$eval('.login-submit', el => el.click());
-        // await page.screenshot({ path: './tests/img/special-characters-in-login/sign-in-input3.png' });
-        // // attendre que l'élément <body> soit chargé
-        // await page.waitForSelector('body')
-        // // récupérer le contenu de l'élément <body>
-        // const html = await page.$eval('body', e => e.innerHTML)
-        // // vérifier que dans cet élément Body on trouve "Polr du campus"
-        // await page.screenshot({path: './tests/img/special-characters-in-login/basic-home.png'});
-        // expect(html).toContain("Polr dev")
+        //await page.$eval('#admin_users_table tr td a.btn-danger', el => el.click());    }, timeout)
+        // on attent que l'élément ".form-control" soit chargé
+        await page.waitForSelector('.content-div')
+        // on saisit les données dans le formulaire Register
+        await page.type('form[action="login"] input[name="username"]',"Vero1%")
+        await page.type('form[action="login"] input[name="password"]',"test")
+        // on clique sur le bouton pour s'enregistrer
+        await page.$eval('.form-control', el => el.click());
+        await page.screenshot({ path: './tests/img/special-characters-in-login/sign-in-input3.png' });
+        // attendre que l'élément <body> soit chargé
+        await page.waitForSelector('body',{delay:50})
+        // récupérer le contenu de l'élément <body>
+        const html = await page.$eval('body', e => e.innerHTML)
+        // vérifier que dans cet élément Body on trouve "Polr du campus"
+        await page.screenshot({path: './tests/img/special-characters-in-login/basic-home.png'});
+        expect(html).toContain("Polr dev")
 
     }, timeout)
 
